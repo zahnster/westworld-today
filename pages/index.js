a4e101660e6e3bf7ec729751963f167e02096466
Jade Pennig <jade@pennig.name>
2018-02-08 10:02:04 -0600

    countdown to westworld i have no patience

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import moment from 'moment'
+
+export default () => (
+  <div className="container">
+    <Head>
+      <link href="https://fonts.googleapis.com/css?family=Raleway:900" rel="stylesheet" />
+    </Head>
+
+    <h1>
+      Westworld season 2 will premiere {moment().set({ year: 2018, month: 4, day: 22, hour: 21}).fromNow()} on April 22nd, 2018, at 9pm, 8pm central.
+    </h1>
+
+    <style jsx global>{`
+      * { padding: 0; margin: 0; box-sizing: border-box; }
+
+      html {
+        height: 100vh;
+        display: flex;
+        align-items: center;
+      }
+
+      .container {
+        padding: 5vw;
+        font-family: "Raleway", sans-serif;
+        font-size: 1.5rem;
+        text-align: center;
+      }
+    `}</style>
+  </div>
+)
