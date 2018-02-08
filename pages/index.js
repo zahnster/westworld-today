a04dfd814d1a62b9405790b77e4b02f9ab86c2d5
Jade Pennig <jade@pennig.name>
2018-02-08 10:42:39 -0600

    get it live

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,41 @@
 import Head from 'next/head'
+import moment from 'moment'
+import ReactGA from 'react-ga'
 
-export default () => (
-  <div className="container">
-    <Head>
-      <link href="https://fonts.googleapis.com/css?family=Raleway:900" rel="stylesheet" />
-    </Head>
+export default class WestworldToday extends React.Component {
+  componentDidMount() {
+    ReactGA.initialize('UA-113511865-4')
+    ReactGA.pageview('/')
+  }
 
-    <h1>
-      Westworld season 2 will premiere on April 22nd, 2018, at 9pm, 8pm central.
-    </h1>
+  render() {
+    return (
+      <div className="container">
+        <Head>
+          <link href="https://fonts.googleapis.com/css?family=Raleway:900" rel="stylesheet" />
+        </Head>
 
-    <style jsx global>{`
-      * { padding: 0; margin: 0; box-sizing: border-box; }
+        <h1>
+          Westworld season 2 will premiere {moment().set({ year: 2018, month: 4, day: 22, hour: 21}).fromNow()} on April 22nd, 2018, at 9pm, 8pm central.
+        </h1>
 
-      html {
-        height: 100vh;
-        display: flex;
-        align-items: center;
-      }
+        <style jsx global>{`
+          * { padding: 0; margin: 0; box-sizing: border-box; }
 
-      .container {
-        padding: 5vw;
-        font-family: "Raleway", sans-serif;
-        font-size: 1.5rem;
-        text-align: center;
-      }
-    `}</style>
-  </div>
-)
+          html {
+            height: 100vh;
+            display: flex;
+            align-items: center;
+          }
+
+          .container {
+            padding: 5vw;
+            font-family: "Raleway", sans-serif;
+            font-size: 1.5rem;
+            text-align: center;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
